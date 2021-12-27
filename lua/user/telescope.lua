905101a8adde2c0b37625ba35ac3efacf7b89840
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2021-12-27 10:55:34 +0000

    adds vim modeline to telescope.lua

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,3 +1,5 @@
+-- vim: foldmarker=(((,))):foldmethod=marker:foldlevel=0:shiftwidth=2:softtabstop=2:tabstop=2
+
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
   return
