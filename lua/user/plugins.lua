9182036e859884c114e26ccd446179f486c68b3b
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2021-12-27 11:09:35 +0000

    adds gitsigns plugin and config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -135,6 +135,12 @@ return packer.startup(function(use)
 
   -- )))
 
+  -- Git (((
+
+  use("lewis6991/gitsigns.nvim")
+
+  -- )))
+
   -- )))
 
   -- Automatically set up your configuration after cloning packer.nvim (((
