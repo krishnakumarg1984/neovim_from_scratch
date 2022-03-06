91bb7193571da9c14fabd1115905b54bf7aa8330
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-03-06 19:58:15 +0000

    removes codespell requirement in config

--- .vimrc.lua
+++ .vimrc.lua
@@ -13,24 +13,24 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
-null_ls.setup({
+null_ls.setup {
   -- you can reuse a shared lspconfig on_attach callback here
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+      vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
     end
   end,
   debug = false,
   sources = {
     -- formatting.clang_format,
-    formatting.codespell,
-    formatting.stylua.with({
+    -- formatting.codespell,
+    formatting.stylua.with {
       condition = function(utils)
-        return utils.root_has_file({ "stylua.toml", ".stylua.toml" })
+        return utils.root_has_file { "stylua.toml", ".stylua.toml" }
       end,
-    }),
-    diagnostics.codespell,
+    },
+    -- diagnostics.codespell,
     -- diagnostics.cppcheck,
     -- diagnostics.cspell,
   },
-})
+}
